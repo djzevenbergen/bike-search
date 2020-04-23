0a020b4b6d5d34da18d087d7e8ce4a714ce29907
DJ Zevenbergen <djzevenbergen@gmail.com>
2020-04-23 14:28:00 -0700

    add styling

--- src/bike-search.js
+++ src/bike-search.js
@@ -1,7 +1,7 @@
 export class BikeSearch {
   async getBikeByZipAndDist(zip, dist) {
     try {
-      let response = await fetch(` https://cors-anywhere.herokuapp.com/bikeindex.org/api/v3/search?stolenness=proximity&location=${zip}&distance=${dist}`);
+      let response = await fetch(`https://cors-anywhere.herokuapp.com/bikeindex.org/api/v3/search?stolenness=proximity&location=${zip}&distance=${dist}`);
       let jsonifiedResponse;
       if (response.ok && response.status == 200) {
         jsonifiedResponse = await response.json();
@@ -14,5 +14,21 @@ export class BikeSearch {
       return false;
     }
   }
-
+  async getBikeStolenTotal(zip, dist) {
+    try {
+      let response = await fetch(`https://cors-anywhere.herokuapp.com/bikeindex.org/api/v3/search/count?stolenness=proximity&location=${zip}&distance=${dist}`);
+      let jsonifiedResponse;
+      if (response.ok && response.status == 200) {
+        jsonifiedResponse = await response.json();
+      } else {
+        jsonifiedResponse = false;
+      }
+      return jsonifiedResponse
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
 }
+
+
--- src/main.js
+++ src/main.js
@@ -4,30 +4,37 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from 'jquery';
 
+
+
+
 $(document).ready(function () {
 
   $("#search-form").submit(function (event) {
     event.preventDefault();
+    $("#output").text('');
 
     let zip = $('#zipcode').val();
     let dist = $("#distance").val();
+
     $("#zipcode").val("");
     $("#distance").val("");
 
     console.log(zip);
     console.log(dist);
+
     (async () => {
       let bikeSearch = new BikeSearch();
       const response = await bikeSearch.getBikeByZipAndDist(zip, dist);
-      getElements(response);
+      const responseTwo = await bikeSearch.getBikeStolenTotal(zip, dist);
+      getElementsForColumn(response);
+      getElementsForCount(responseTwo);
     })();
-
-    function getElements(response) {
+    function getElementsForColumn(response) {
       if (response) {
         $("#location").text(`These bikes are within ${dist} miles of ${zip}`);
         console.log(response);
         response.bikes.forEach(function (bike) {
-          $("#output").append('<p><a href="' + bike.url + '" target="_blank">' + bike.url + '</a></p><br>');
+          $("#output").append('<li class="bike"><a href="' + bike.url + '" target="_blank">' + bike.url + '</a><img src="' + bike.thumb + '" alt="no image available"></li><br>');
         });
 
       } else {
@@ -35,7 +42,15 @@ $(document).ready(function () {
       }
     }
 
-
+    function getElementsForCount(responseTwo) {
+      if (responseTwo.proximity > 1) {
+        $("#count").text(`There are ${responseTwo.proximity} stolen bikes in ${zip}`);
+        console.log(responseTwo);
+      } else if (responseTwo.proximity == 1) {
+        $("#count").text(`There is ${responseTwo.proximity} stolen bike in ${zip}`);
+      } else {
+        $("#count").text('Ooops there was an error')
+      }
+    }
   });
-
 });
