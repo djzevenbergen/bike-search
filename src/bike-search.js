12910fd201ba987b37417efe1775e65515715885
DJ Zevenbergen <djzevenbergen@gmail.com>
2020-04-23 11:34:34 -0700

    add listing functionality

---
+++ src/bike-search.js
@@ -0,0 +1,18 @@
+export class BikeSearch {
+  async getBikeByZipAndDist(zip, dist) {
+    try {
+      let response = await fetch(` https://cors-anywhere.herokuapp.com/bikeindex.org/api/v3/search?stolenness=proximity&location=${zip}&distance=${dist}`);
+      let jsonifiedResponse;
+      if (response.ok && response.status == 200) {
+        jsonifiedResponse = await response.json();
+      } else {
+        jsonifiedResponse = false;
+      }
+      return jsonifiedResponse;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+
+}
--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
+import { BikeSearch } from './bike-search.js';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
 import $ from 'jquery';
 
 $(document).ready(function () {
@@ -5,10 +9,32 @@ $(document).ready(function () {
   $("#search-form").submit(function (event) {
     event.preventDefault();
 
-    let key = process.env.KEY;
-    let koy = process.env.KOY;
-    console.log(key);
-    console.log(koy);
+    let zip = $('#zipcode').val();
+    let dist = $("#distance").val();
+    $("#zipcode").val("");
+    $("#distance").val("");
+
+    console.log(zip);
+    console.log(dist);
+    (async () => {
+      let bikeSearch = new BikeSearch();
+      const response = await bikeSearch.getBikeByZipAndDist(zip, dist);
+      getElements(response);
+    })();
+
+    function getElements(response) {
+      if (response) {
+        $("#location").text(`These bikes are within ${dist} miles of ${zip}`);
+        console.log(response);
+        response.bikes.forEach(function (bike) {
+          $("#output").append('<p><a href="' + bike.url + '" target="_blank">' + bike.url + '</a></p><br>');
+        });
+
+      } else {
+        $("#location").text("Oh no! There was an error!");
+      }
+    }
+
 
   });
 
